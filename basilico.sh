163a44eecf680b383eaa7d844eee8509eba67021
Andrey Ilatovskiy <Andreyyiv@users.noreply.github.com>
2026-05-10 00:51:39 -0400

    major revision
    * robust handling of variables and file paths
    * fixed errors

--- basilico.sh
+++ basilico.sh
@@ -1,71 +1,71 @@
+#!/usr/bin/env bash
+
 ### BaSiLiCo local interface
 
 echo
-echo "Info: BaSiLiCo v1, copyright 2019 Andrey Ilatovskiy"
+echo 'BaSiLiCo v1 (C) 2014-2018, 2026 Andrey Ilatovskiy'
 echo
-echo "THERE IS NO WARRANTY FOR THE PROGRAM, TO THE EXTENT PERMITTED BY APPLICABLE LAW."
-echo "EXCEPT WHEN OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES"
-echo "PROVIDE THE PROGRAM 'AS IS' WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED,"
-echo "INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS"
-echo "FOR A PARTICULAR PURPOSE. THE ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE"
-echo "PROGRAM IS WITH YOU. SHOULD THE PROGRAM PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL"
-echo "NECESSARY SERVICING, REPAIR OR CORRECTION."
+echo 'THERE IS NO WARRANTY FOR THE PROGRAM, TO THE EXTENT PERMITTED BY APPLICABLE LAW.'
+echo 'EXCEPT WHEN OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES'
+echo 'PROVIDE THE PROGRAM "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED,'
+echo 'INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS'
+echo 'FOR A PARTICULAR PURPOSE. THE ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE'
+echo 'PROGRAM IS WITH YOU. SHOULD THE PROGRAM PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL'
+echo 'NECESSARY SERVICING, REPAIR OR CORRECTION.'
 echo
-echo "IN NO EVENT UNLESS REQUIRED BY APPLICABLE LAW OR AGREED TO IN WRITING WILL ANY COPYRIGHT"
-echo "HOLDER, OR ANY OTHER PARTY WHO MODIFIES AND/OR CONVEYS THE PROGRAM AS PERMITTED ABOVE,"
-echo "BE LIABLE TO YOU FOR DAMAGES, INCLUDING ANY GENERAL, SPECIAL, INCIDENTAL OR CONSEQUENTIAL"
-echo "DAMAGES ARISING OUT OF THE USE OR INABILITY TO USE THE PROGRAM (INCLUDING BUT NOT"
-echo "LIMITED TO LOSS OF DATA OR DATA BEING RENDERED INACCURATE OR LOSSES SUSTAINED BY YOU"
-echo "OR THIRD PARTIES OR A FAILURE OF THE PROGRAM TO OPERATE WITH ANY OTHER PROGRAMS), EVEN"
-echo "IF SUCH HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES."
+echo 'IN NO EVENT UNLESS REQUIRED BY APPLICABLE LAW OR AGREED TO IN WRITING WILL ANY COPYRIGHT'
+echo 'HOLDER, OR ANY OTHER PARTY WHO MODIFIES AND/OR CONVEYS THE PROGRAM AS PERMITTED ABOVE,'
+echo 'BE LIABLE TO YOU FOR DAMAGES, INCLUDING ANY GENERAL, SPECIAL, INCIDENTAL OR CONSEQUENTIAL'
+echo 'DAMAGES ARISING OUT OF THE USE OR INABILITY TO USE THE PROGRAM (INCLUDING BUT NOT'
+echo 'LIMITED TO LOSS OF DATA OR DATA BEING RENDERED INACCURATE OR LOSSES SUSTAINED BY YOU'
+echo 'OR THIRD PARTIES OR A FAILURE OF THE PROGRAM TO OPERATE WITH ANY OTHER PROGRAMS), EVEN'
+echo 'IF SUCH HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.'
 echo
 
-### confirm that the script is being executed with bash 
-if [ -n "$BASH_VERSION" ]; then
-    echo "Info: Executed with bash version $BASH_VERSION"
+### confirm that the script is being executed with bash
+if [ -n "${BASH_VERSION}" ] ; then
+    echo "Info: bash version: ${BASH_VERSION}"
 else
-    echo "Error: this script invokes bash-specific commands, please execute using bash"
-    echo
+    echo 'Error: this script invokes bash-specific commands, please execute using bash'
     exit 1
 fi
 
-export BASILICO_ROOT="$(dirname "$(readlink -f -n "$0")")"
+BASILICO_ROOT="$(dirname "$(readlink -f -n "$0")")"
+export BASILICO_ROOT
 
 ### default values for input arguments
 IFILE=
 ODIR=
 OBJECTMODE=
-LIMITMEMORY='true'
 GENERATEWEB='true'
 
 echo
 
 function show_help {
   echo 'Usage:'
-  echo 'bash '$BASILICO_ROOT'/basilico.sh -i input.icb -d output/ [-o -w]'
-  echo 'bash '$BASILICO_ROOT'/basilico.sh -h'
+  echo "./${BASILICO_ROOT}/basilico.sh -i input.icb -d output/ [-o -w]"
+  echo "./${BASILICO_ROOT}/basilico.sh -h"
   echo '  -o = object mode [default: ensemble mode]'
   echo '  -w = do not generate web files'
   echo '  -h = show this help message'
   echo
-  exit $1
+  exit "$1"
 }
 
 ### parse arguments
-while getopts ":i:d:oMwh" opt; do
-  case $opt in
-    i) IFILE=$OPTARG ;;
-    d) ODIR=$OPTARG ;;
-    o) OBJECTMODE='ob' ;;
-    M) LIMITMEMORY='false' ;;
-    w) GENERATEWEB='false' ;;
-    h) show_help 0 ;;
-    \?)
-      echo "Invalid option: -$OPTARG" >&2
+while getopts ":i:d:owh" OPT; do
+  case "${OPT}" in
+    'i') IFILE="${OPTARG}" ;;
+    'd') ODIR="${OPTARG}" ;;
+    'o') OBJECTMODE='ob' ;;
+    'w') GENERATEWEB='false' ;;
+    'h') show_help 0 ;;
+    '?')
+      echo "Invalid option: -${OPTARG}" >&2
       show_help 1
       ;;
-    :)
-      echo "Option -$OPTARG requires an argument." >&2
+    ':')
+      echo "Option -${OPTARG} requires an argument." >&2
       show_help 1
       ;;
   esac
@@ -73,15 +73,15 @@ done
 shift $((OPTIND - 1)) # pop parsed arguments, leave the remaining arguments
 
 ### confirm that IFILE and ODIR are defined
-[ "x${IFILE}" == 'x' ] && show_help 0
-[ "x${ODIR}"  == 'x' ] && show_help 0
+[[ "${IFILE}" == '' ]] && show_help 0
+[[ "${ODIR}"  == '' ]] && show_help 0
 
 ### canonicalize paths, resolve symlinks for the input file and output dir
 IFILE="$(readlink -f -n "${IFILE}")"
 ODIR="$(readlink -m -n "${ODIR}")"
 
 ### define the names for the output files
-NAME=$(basename "${IFILE}" .icb)
+NAME="$(basename "${IFILE}" .icb)"
 TAGGED="${ODIR}/${NAME}_tag.icb"
 TLIG="${ODIR}/${NAME}_tlig.icb"
 STRE="${ODIR}/${NAME}_stre.icb"
@@ -92,38 +92,36 @@ TSV="${ODIR}/${NAME}.tsv"
 ### execute
 (
 
-# similar to the Pocketome web server
-[ "x${LIMITMEMORY}" == 'xtrue' ] && ulimit -m 6000000 ### 2025-12-26 IK - used to be 600000 but this is not enough to start icm
+mkdir -pv -- "${ODIR}" || exit 1
 
-mkdir -pv -- ${ODIR} || exit 1
-
-rm -f -- ${TAGGED} ${TLIG} ${STRE} ${SVGROOT} ${HTML} ${TSV}
+rm -f -- "${TAGGED}" "${TLIG}" "${STRE}" "${SVGROOT}" "${HTML}" "${TSV}" || exit 1
 
 set -x
 
 echo
-icm $BASILICO_ROOT/scripts/basilico/settags.icm if=${IFILE} of=${TAGGED} || exit 1
-[ -e ${TAGGED} ] || exit 1
+icm "${BASILICO_ROOT}/scripts/basilico/settags.icm" if="${IFILE}" of="${TAGGED}" || exit 1
+[[ -e "${TAGGED}" ]] || exit 1
 
 echo
-icm $BASILICO_ROOT/scripts/basilico/ligclustering.icm if=${TAGGED} of=${TLIG} ${OBJECTMODE} || exit 1
-[ -e ${TLIG} ] || exit 1
+icm "${BASILICO_ROOT}/scripts/basilico/ligclustering.icm" if="${TAGGED}" of="${TLIG}" "${OBJECTMODE}" || exit 1
+[[ -e "${TLIG}" ]] || exit 1
 
 echo
-icm $BASILICO_ROOT/scripts/basilico/stre.icm if=${TAGGED} tlig=${TLIG} of=${STRE} ${OBJECTMODE} || exit 1
-[ -e ${STRE} ] || exit 1
+icm "${BASILICO_ROOT}/scripts/basilico/stre.icm" if="${TAGGED}" tlig="${TLIG}" of="${STRE}" "${OBJECTMODE}" || exit 1
+[[ -e "${STRE}" ]] || exit 1
 
 echo
-[ "x${GENERATEWEB}" == 'xfalse' ] && exit 0
+[[ "${GENERATEWEB}" == 'false' ]] && exit 0
 
 echo
-icm $BASILICO_ROOT/scripts/basilico/mkweb.icm if=${STRE} osvg=${SVGROOT} ohtml=${HTML} otsv=${TSV} || exit 1
-[ -e ${SVGROOT} ] || exit 1
+icm "${BASILICO_ROOT}/scripts/basilico/mkweb.icm" if="${STRE}" osvg="${SVGROOT}" ohtml="${HTML}" otsv="${TSV}" || exit 1
+[[ -e "${SVGROOT}" ]] || exit 1
 
 set +x
 
 ) || exit 1
 
 echo
-echo 'Info: BaSiLiCo completed successfuflly'
+echo 'Info: BaSiLiCo completed successfully'
 echo
+
